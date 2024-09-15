f98beccfe9d67c4200c04482e3cb876c68edda86
serenAskin <serenaskin@hotmail.com>
2024-09-15 22:09:04 +0300

    DevOps Starup
    DevOps Starup

---
+++ src/main/java/com/serenaskin/controller/DevOpsController.java
@@ -0,0 +1,24 @@
+package com.serenaskin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+
+    //  http://localhost:8080
+    @RestController
+    @RequestMapping
+    public class DevOpsController {
+
+        //  http://localhost:8080
+        @GetMapping
+        public String devopsHello() {
+            return "DevOps Hello : " + LocalDateTime.now();
+        }
+
+        //  http://localhost:8080/info
+        @GetMapping("/info")
+        public String info() {
+            return "DEVOPS INFO : " + LocalDateTime.now();
+        }
+    }
---
+++ src/test/java/com/serenaskin/AppMainTests.java
@@ -0,0 +1,13 @@
+package com.serenaskin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
